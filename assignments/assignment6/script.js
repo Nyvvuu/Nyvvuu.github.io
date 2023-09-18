e25a6e969591d1c1e9c0c9397b11ad71c5196b00
Nyla <nyvu6424@gmail.com>
2023-09-18 16:50:14 -0400

    style update

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,13 +1,12 @@
 const bounceBall = () => {
     let ball = document.getElementById("ball");
-    ball.classList.add("bounce");
     
-    // Listen for the "animationend" event to remove the class after the animation completes
-    ball.addEventListener("animationend", () => {
-        ball.classList.remove("bounce");
-    }, { once: true }); // { once: true } ensures that the event listener runs only once
+    // Remove any existing animation classes
+    ball.classList.remove("bounce");
+    
+    // Trigger a reflow to reset the animation
+    void ball.offsetWidth;
+    
+    // Add the animation class back to start the animation
+    ball.classList.add("bounce");
 }
-
-window.onload = () => {
-    document.getElementById("bounce-button").onclick = bounceBall;
-};
