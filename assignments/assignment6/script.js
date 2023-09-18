012e566c0e941599b9261abc740667a9e83378bf
Nyla <nyvu6424@gmail.com>
2023-09-18 17:57:42 -0400

    style update

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -16,19 +16,3 @@ const bounceBall = () => {
    window.onload = () => {
     document.getElementById("bounce-button").onclick=bounceBall;
    };
-
-/* hide and show */
-const image = document.getElementById("image");
-const show = document.getElementById("show");
-const hide = document.getElementById("hide");
-
-// Add click event listeners to the show and hide buttons
-showButton.addEventListener("click", () => {
-    // Set the image's display property to "block" to show it
-    image.style.display = "block";
-});
-
-hideButton.addEventListener("click", () => {
-    // Set the image's display property to "none" to hide it
-    image.style.display = "none";
-});
