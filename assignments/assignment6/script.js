fec8dbe638eab8f989630ec8daebb0ca572ec58a
Nyla <nyvu6424@gmail.com>
2023-09-18 16:51:13 -0400

    style update

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,12 +1,21 @@
+let animationInProgress = false; // Add a flag to track animation state
+
 const bounceBall = () => {
-    let ball = document.getElementById("ball");
-    
-    // Remove any existing animation classes
-    ball.classList.remove("bounce");
-    
-    // Trigger a reflow to reset the animation
-    void ball.offsetWidth;
-    
-    // Add the animation class back to start the animation
-    ball.classList.add("bounce");
+    if (!animationInProgress) {
+        let ball = document.getElementById("ball");
+        ball.classList.add("bounce");
+        
+        animationInProgress = true; // Set the flag to true to prevent further animation until it ends
+        
+        // Listen for the "animationend" event to remove the class after the animation completes
+        ball.addEventListener("animationend", () => {
+            ball.classList.remove("bounce");
+            animationInProgress = false; // Reset the flag after animation ends
+        }, { once: true }); // { once: true } ensures that the event listener runs only once
+    }
 }
+
+window.onload = () => {
+    document.getElementById("bounce-button").onclick = bounceBall;
+};
+
