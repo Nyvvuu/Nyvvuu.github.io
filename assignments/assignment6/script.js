d342174706e494fa18ba52f77bfdf3d18d866309
Nyla <nyvu6424@gmail.com>
2023-09-18 17:13:21 -0400

    updated color change

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,4 +1,14 @@
-const bounceBall =()=>{
-    const ballElement =document.getElementById("ball");
+const startBouncing = () => {
+    const ballElement = document.getElementById("ball");
+    
+    // Add the "bounce" class to start the animation
     ballElement.classList.add("bounce");
+    
+    // Listen for the "animationend" event to remove the class after the animation completes
+    ballElement.addEventListener("animationend", () => {
+        ballElement.classList.remove("bounce");
+    }, { once: true }); // { once: true } ensures that the event listener runs only once
 };
+
+// Attach the click event listener to the "bounce-button"
+document.getElementById("bounce-button").onclick = startBouncing;
