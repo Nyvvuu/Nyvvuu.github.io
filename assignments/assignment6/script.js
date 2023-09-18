2bf373b9204e2fdd11f66a3776fb82f81af4efe9
Nyla <nyvu6424@gmail.com>
2023-09-18 16:49:22 -0400

    additional assignment

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,12 +1,13 @@
-const bounceBall =() =>{
+const bounceBall = () => {
     let ball = document.getElementById("ball");
     ball.classList.add("bounce");
-}
-ball.addEventListener("animationend", ()=>{
-    ball.classList.remove("bounce");
-},{once:true});
-
-window.onload=()=>{
-    document.getElementById("bounce-button").onclick = bounceBall;  
     
+    // Listen for the "animationend" event to remove the class after the animation completes
+    ball.addEventListener("animationend", () => {
+        ball.classList.remove("bounce");
+    }, { once: true }); // { once: true } ensures that the event listener runs only once
+}
+
+window.onload = () => {
+    document.getElementById("bounce-button").onclick = bounceBall;
 };
