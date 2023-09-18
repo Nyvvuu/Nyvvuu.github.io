e583a0aee378aff4097dfc462d1a9333987bc5cc
Nyla <nyvu6424@gmail.com>
2023-09-18 17:01:08 -0400

    updated color change

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,8 +1,3 @@
-const bounceBall =() =>{
-    let ballElement = document.getElementById("ball");
-    ballElement.classList.add("bounce");
-};
-window.onload = () => {
-    document.getElementById("bounce-button").onclick = bounceBall;  
-    
-};
+window.onload = () => {};
+
+const bounceButton=document.getElementById("bounce-Button");
