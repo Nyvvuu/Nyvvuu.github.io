89cf9d473396ac5c6f77a01f7a1139b74bc02d8d
Nyla <nyvu6424@gmail.com>
2023-09-25 08:40:48 -0400

    style update

--- assignments/assignment7/script.js
+++ assignments/assignment7/script.js
@@ -24,3 +24,40 @@ window.onload=()=>{
 
 
 /*Who is the oldest code*/
+const fundraising = () => {
+    // Get values from input fields
+    const age1 = parseInt(document.getElementById("text-age1").value);
+    const name1 = document.getElementById("text-name1").value;
+    const age2 = parseInt(document.getElementById("text-age2").value);
+    const name2 = document.getElementById("text-name2").value;
+    const age3 = parseInt(document.getElementById("text-age3").value);
+    const name3 = document.getElementById("text-name3").value;
+
+    // Sets the errors
+    const error = document.getElementById("error-num");
+    const orderDiv = document.getElementById("coffee-order");
+
+    // Hide error and order elements initially
+    error.classList.add("hidden");
+    orderDiv.classList.add("hidden");
+
+    // Checks for errors via an if statement
+    if (isNaN(age1) || isNaN(age2) || isNaN(age3)) {
+        error.classList.remove("hidden");
+        error.innerHTML = "* Not a valid number";
+    }
+    else if (age1 < 0 || age2 < 0 || age3 < 0) {
+        error.classList.remove("hidden");
+        error.innerHTML = "* Negative Number";
+    }
+    else if (age1 === 0 && age2 === 0 && age3 === 0) {
+        error.classList.remove("hidden");
+        error.innerHTML = "* Can't order zero coffees";
+    }
+    
+    // Display ages and names
+    else {
+        orderDiv.innerHTML = `List of names and ages: ${name1} (${age1}), ${name2} (${age2}), ${name3} (${age3}).`;
+        orderDiv.classList.remove("hidden");
+    }
+}
